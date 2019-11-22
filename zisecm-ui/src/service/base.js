552974e91fa1114abe4aaa71c83099cdb90e3ca4
lanseyouling163 <55675605+lanseyouling163@users.noreply.github.com>
2019-11-22 23:16:32 +0800

    Update base.js

--- zisecm-ui/src/service/base.js
+++ zisecm-ui/src/service/base.js
@@ -1,3 +1,10 @@
+/**
+ * 服务基类
+ * 处理最基本的增删改查
+ * 使用的前提条件：
+ * 1.在初始化时都需要定义一个数据的路径，如用户的/user,文件夹的/folder
+ * 2.所有继承的子类在调用url上是有一定规律，如都有save、get、delete、update等相同名称的方法
+ */
 class BaseService{
     constructor(baseurl){
         this.baseurl = baseurl;
@@ -6,23 +13,28 @@ class BaseService{
     get(id,callback){
         let params={id:id};
         let url = this.baseurl+"/get"
-        if(undefined != callback && typeof callback == 'function'){
-            callback({msg:"abc"});
-        }
+        axios.post(url,params).then(function(response){
+          if(undefined != callback && typeof callback == 'function'){
+            callback(response.data);
+          }
+        })
     }
     save(obj,callback){
         let url = this.baseurl+"/save"
-        console.log(url)
-        if(undefined != callback && typeof callback == 'function'){
-           callback({msg:"abc"});
-        }
+        axios.post(url,obj).then(function(response){
+          if(undefined != callback && typeof callback == 'function'){
+            callback(response.data);
+          }
+        })
     }
     delete(id,callback){
         let params={id:id};
         let url = this.baseurl+"/delete"
-        if(undefined != callback && typeof callback == 'function'){
-            callback({msg:"abc"});
-        }
+        axios.post(url,params).then(function(response){
+          if(undefined != callback && typeof callback == 'function'){
+            callback(response.data);
+          }
+        })
     }
 }
 
