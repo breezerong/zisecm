a4fa901249a4c37b354d67beed4a6a94e3680325
liaojiyongde <liaojiyongde@qq.com>
2019-11-26 23:34:30 +0800

    flowable

--- flowable/src/test/java/org/flowable/designer/test/ProcessTestMyProcess.java
+++ flowable/src/test/java/org/flowable/designer/test/ProcessTestMyProcess.java
@@ -12,14 +12,22 @@ import org.flowable.engine.repository.Deployment;
 import org.flowable.engine.repository.DeploymentProperties;
 import org.flowable.engine.runtime.ProcessInstance;
 import org.flowable.engine.test.FlowableRule;
+import org.flowable.task.api.Task;
 import org.junit.Rule;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import com.ecm.core.bpm.WorkflowAuditService;
+import com.ecm.core.bpm.WorkitemService;
+import com.ecm.core.cache.manager.SessionManager;
+import com.ecm.core.entity.EcmUser;
+import com.ecm.core.entity.LoginUser;
+import com.ecm.core.service.UserService;
 import com.ecm.flowable.controller.ExpenseController;
 @Configurable
 @Configuration
@@ -27,7 +35,11 @@ import com.ecm.flowable.controller.ExpenseController;
  public class ProcessTestMyProcess {
 
 	private String filename = "C:\\Workfolder\\zisecm\\flowable\\src\\main\\resources\\diagrams\\借阅流程.bpmn";
- 
+	@Autowired
+	private WorkflowAuditService workflowAuditService;
+	@Autowired
+	private UserService userService;
+	
 //	@Test
 //	public void startProcess() throws Exception {
 //		
@@ -53,13 +65,30 @@ import com.ecm.flowable.controller.ExpenseController;
 	@Test
 	public void startProcess() throws Exception {
 		RepositoryService repositoryService = activitiRule.getRepositoryService();
-		repositoryService.createDeployment().addInputStream("借阅流程.bpmn", new FileInputStream(filename)).deploy();
+		//repositoryService.createDeployment().addInputStream("借阅流程.bpmn", new FileInputStream(filename)).deploy();
 		RuntimeService runtimeService = activitiRule.getRuntimeService();
 		Map<String, Object> variableMap = new HashMap<String, Object>();
 		variableMap.put("name", "Activiti");
+		variableMap.put("formID", "xxxxx");//流程发起时，将表单ID写入流程中
 		ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("process_borrow", variableMap);
-		assertNotNull(processInstance.getId());
-		System.out.println("ProcessId=============================== " + processInstance.getId() + "  ProcessDefinitionId=============================== "
-				+ processInstance.getProcessDefinitionId());
+		//process id 
+		//workflowAuditService.newObject(token, obj)
+		EcmUser ecmUser= new EcmUser();
+		ecmUser.setLoginName("admin");
+		ecmUser.setLoginName("Passw0rd");
+		LoginUser loginUser=  userService.authentication(ecmUser);
+  		//		activitiRule.getRuntimeService().createProcessInstanceQuery().processInstanceId("45005").singleResult().getProcessVariables()
+		//activitiRule.getTaskService().createTaskQuery().processInstanceId("57505").list();
+//		Task task =activitiRule.getTaskService().createTaskQuery().taskId("45014").singleResult().getProcessVariables();
+//		task.getTaskLocalVariables()
+//        if (task == null) {
+//            throw new RuntimeException("流程不存在");
+//        }
+//        map.put("outcome", "通过");
+//        activitiRule.getTaskService().complete("");
+        
+//		assertNotNull(processInstance.getId());
+//		System.out.println("ProcessId=============================== " + processInstance.getId() + "  ProcessDefinitionId=============================== "
+//				+ processInstance.getProcessDefinitionId());
 	}
 }
--- zisecm-ui/src/views/test/TestComp2.vue
+++ zisecm-ui/src/views/test/TestComp2.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
     测试2
+    <el-form label-width="120px" @submit.native.prevent>
+      <el-row>
+         <el-col :span="8">
+           起流程
+         </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="8">
+          <el-form-item label="表单">
+            <el-input type="text"  v-model="wfData.formId" ></el-input>
+          </el-form-item>
+        </el-col>
+         <el-col :span="8">
+           <el-button type="primary" plain icon="save" @click="startWorkflow()">启动</el-button> 
+         </el-col>
+      </el-row>
+    </el-form>
   </div>
 </template>
 
@@ -11,12 +28,27 @@ export default {
   permit: 1,
   data() {
     return {
-      
+      wfData: {
+        formId:""
+      },
       formLabelWidth: "120px"
     };
   },
   methods: {
-    
+    startWorkflow(){
+      let _self = this;
+      _self.loading =true;
+      let m = new  Map();
+      m.set("processId",_self.wfData.formId);
+      m.set("name","2");
+      axios.post("/workflow/startWrokflow",JSON.stringify(m)).then(function(response){
+        console.log(response);  
+        _self.loading = false;
+      }).catch(function(error){
+        console.log(error);
+        _self.loading = false;
+      });
+    }
   }
 };
 </script>
