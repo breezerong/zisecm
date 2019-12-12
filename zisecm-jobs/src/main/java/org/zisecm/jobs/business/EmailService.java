5d5d5843a80b50f00d0f875c3346ad26c4239794
tr1314qq <tr1314qq@163.com>
2019-12-12 20:51:05 +0800

    Update EmailService.java

--- zisecm-jobs/src/main/java/org/zisecm/jobs/business/EmailService.java
+++ zisecm-jobs/src/main/java/org/zisecm/jobs/business/EmailService.java
@@ -2,7 +2,9 @@ package org.zisecm.jobs.business;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -10,14 +12,26 @@ import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.zisecm.jobs.core.Ijobs;
 
+import com.ecm.common.util.PasswordUtils;
+import com.ecm.core.ActionContext;
+import com.ecm.core.cache.manager.CacheManagerOper;
+import com.ecm.core.entity.EcmAttribute;
+import com.ecm.core.service.AuthService;
+import com.ecm.core.service.DocumentService;
 import com.ecm.core.service.MailService;
 @Service
 public class EmailService implements Ijobs {
+	private String token;
 	/**
      * 邮件发送测试类
      */
     @Autowired
     private MailService mailService;
+    @Autowired
+    private DocumentService documentService;
+    
+    @Autowired
+	private AuthService authService;
     
     @Autowired
     private SpringTemplateEngine templateEngine;
@@ -51,7 +65,58 @@ public class EmailService implements Ijobs {
 	 */
 	@Scheduled(cron = "0/10 * * * * *")
 	public synchronized void runJobs() throws Exception {
+		
+		String sql="select "+getDocumentAllColumns()+" from ecm_document where ";
+		documentService.executeSQL(getToken(), "");
 		sendMail();
+	}
+	
+	/**
+	 * 获取文档所有列
+	 * @return
+	 */
+	private String getDocumentAllColumns() {
+		String cols = "";
+		for(Entry<String, EcmAttribute> attr :CacheManagerOper.getEcmAttributes().entrySet()) {
+			if(cols.length()==0) {
+				cols = attr.getValue().getName();
+			}else {
+				cols += ","+attr.getValue().getName();
+			}
+		}
+		return cols;
+	}
+	
+	public String getToken() {
+		if(token==null || authService.login(token)==null) {
+			String user = null;
+			try {
+				user = CacheManagerOper.getEcmParameters().get("IndexUser").getValue();
+
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				// e.printStackTrace();
+				return null;
+			}
+			
+			String password = CacheManagerOper.getEcmParameters().get("IndexPassword").getValue();
+			if(!StringUtils.isEmpty(password)) {
+				try {
+					password = PasswordUtils.decodePassword(password);
+				}
+				catch(Exception ex) {
+					
+				}
+			}
+			try {
+				token = authService.login(ActionContext.APP_INDEX_AGENT,user,password).getToken();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}
+		}
+		return token;
 	}
 	
 	@Override
