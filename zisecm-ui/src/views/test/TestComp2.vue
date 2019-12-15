2b4bd8995bd4c68a2068c67b2e09e0e88b455a77
breezerong <nstorm@163.com>
2019-12-15 20:49:51 +0800

    测试流程恢复

--- zisecm-ui/src/views/test/TestComp2.vue
+++ zisecm-ui/src/views/test/TestComp2.vue
@@ -6,7 +6,29 @@
     <el-row>
       <el-input type="textarea" :rows="6" v-model="optionMessage"></el-input>
     </el-row>
+    <el-row>
+      <el-form label-width="120px" @submit.native.prevent>
+      <el-row>
+         <el-col :span="8">
+           起流程
+         </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="8">
+          <el-form-item label="表单">
+            <el-input type="text"  v-model="wfData.formId" ></el-input>
+          </el-form-item>
+        </el-col>
+         <el-col :span="8">
+           <el-button type="primary" plain icon="save" @click="deployProcess()">发布流程</el-button> 
+           <el-button type="primary" plain icon="save" @click="startWorkflow()">启动流程</el-button> 
+           <el-button type="primary" plain icon="save" @click="testWorkflow()">完成任务</el-button> 
+         </el-col>
+      </el-row>
+    </el-form>
+    </el-row>
   </div>
+  
 </template>
 
 <script type="text/javascript">
@@ -30,6 +52,45 @@ export default {
       _self.loading =true;
       axios.post("/tools/updateDepartment").then(function(response){
         _self.optionMessage = response.data.data;
+        _self.loading = false;
+      }).catch(function(error){
+        console.log(error);
+        _self.loading = false;
+      });
+    },
+    startWorkflow(){
+      let _self = this;
+      _self.loading =true;
+      let m = new  Map();
+      m.set("formId",_self.wfData.formId);
+      axios.post("/workflow/startWorkflow",JSON.stringify(m)).then(function(response){
+        console.log(response);  
+        _self.loading = false;
+      }).catch(function(error){
+        console.log(error);
+        _self.loading = false;
+      });
+    },
+    testWorkflow(){
+      let _self = this;
+      _self.loading =true;
+      let m = new  Map();
+      m.set("formId",_self.wfData.formId);
+      axios.post("/workflow/testWorkflow",JSON.stringify(m)).then(function(response){
+        console.log(response);  
+        _self.loading = false;
+      }).catch(function(error){
+        console.log(error);
+        _self.loading = false;
+      });
+    },
+      deployProcess(){
+      let _self = this;
+      _self.loading =true;
+      let m = new  Map();
+      m.set("formId",_self.wfData.formId);
+      axios.post("/workflow/deploymentProcess",JSON.stringify(m)).then(function(response){
+        console.log(response);  
         _self.loading = false;
       }).catch(function(error){
         console.log(error);
