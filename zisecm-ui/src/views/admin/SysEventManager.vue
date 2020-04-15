5b25228b7e646834ee5819a2c2f0937de423078b
breezerong <nstorm@163.com>
2020-04-15 16:45:19 +0800

    系统事件

---
+++ zisecm-ui/src/views/admin/SysEventManager.vue
@@ -0,0 +1,212 @@
+<template>
+  <div>
+    <el-dialog title="添加" :visible.sync="dialogVisible">
+      <el-form :model="form">
+        <el-form-item label="名称" :label-width="formLabelWidth">
+          <el-input v-model="form.name" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="说明" :label-width="formLabelWidth">
+          <el-input v-model="form.description" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="是否启用" :label-width="formLabelWidth">
+          <el-select v-model="form.enabled">
+            <el-option label="是" :value="true"></el-option>
+            <el-option label="否" :value="false"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="newItem(form)">确 定</el-button>
+      </div>
+    </el-dialog>
+    <el-container>
+      <el-header>
+        <el-breadcrumb separator="/" class="navbar">
+          <el-breadcrumb-item>系统管理</el-breadcrumb-item>
+          <el-breadcrumb-item>日志管理</el-breadcrumb-item>
+          <el-breadcrumb-item>事件管理</el-breadcrumb-item>
+        </el-breadcrumb>
+        <el-row class="topbar">
+          <el-col :span="4">
+            <el-input
+              v-model="inputkey"
+              placeholder="请输入关键字"
+              @change="search"
+              prefix-icon="el-icon-search"
+            ></el-input>
+          </el-col>
+          <el-col :span="20" style="text-align:left;">
+            &nbsp; 
+            <el-button
+              type="primary"
+              icon="el-icon-edit"
+              plain
+              @click="dialogVisible = true"
+            >新建</el-button>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-main>
+        <el-table
+          :data="dataList"
+          border
+          :height="tableHeight"
+          v-loading="loading"
+          style="width: 100%"
+        >
+          <el-table-column label="行号" type="index" width="60"></el-table-column>
+          <el-table-column label="名称" prop="name" min-width="20%" sortable>
+          </el-table-column>
+          <el-table-column label="说明" min-width="30%">
+            <template slot-scope="scope">
+              <el-input v-model="scope.row.description"></el-input>
+            </template>
+          </el-table-column>
+          <el-table-column label="是否启用" width="120">
+            <template slot-scope="scope">
+              <el-select v-model="scope.row.enabled">
+                <el-option label="是" :value="true"></el-option>
+                <el-option label="否" :value="false"></el-option>
+              </el-select>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作" width="160">
+            <template slot-scope="scope">
+              <el-button
+                :plain="true"
+                type="primary"
+                size="small"
+                icon="edit"
+                @click="saveItem(scope.row)"
+              >保存</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script type="text/javascript">
+// $.ajaxSetup({
+//   contentType: "application/json"
+// });
+
+export default {
+  name: "SysEventManager",
+  permit: 9,
+  data() {
+    return {
+      dataList: [],
+      dataListFull: [],
+      inputkey: "",
+      loading: false,
+      dialogVisible: false,
+      tableHeight: window.innerHeight - 115,
+      form: {
+        name: "",
+        description: "",
+        enabled: true
+      },
+      formLabelWidth: "120px"
+    };
+  },
+  mounted() {
+    this.refreshData();
+  },
+  methods: {
+    refreshData() {
+      let _self = this;
+      _self.loading = true;
+      axios
+        .get("/admin/sysevent")
+        .then(function(response) {
+          _self.dataListFull = response.data.data;
+          _self.dataList = response.data.data;
+          _self.loading = false;
+        })
+        .catch(function(error) {
+          console.log(error);
+          _self.loading = false;
+        });
+    },
+    saveItem(indata) {
+      let _self = this;
+      axios
+        .put("/admin/sysevent", JSON.stringify(indata),{
+        headers: {
+            'Content-Type': 'application/json;charset=UTF-8'
+        }})
+        .then(function(response) {
+          _self.$message("保存成功!");
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    delItem(indata) {
+      let _self = this;
+      axios
+        .delete("/admin/sysevent", indata.id)
+        .then(function(response) {
+          _self.$message("删除成功!");
+          _self.refreshData();
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    newItem(indata) {
+      let _self = this;
+      axios
+        .post("/admin/sysevent", JSON.stringify(indata))
+        .then(function(response) {
+          _self.dialogVisible = false;
+          _self.refreshData();
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    search() {
+      let _self = this;
+      _self.dataList = [];
+      if (_self.inputkey != "" || _self.parentid != "") {
+        _self.dataList = _self.dataListFull.filter(function(item) {
+          return (
+            item.typeName.match(_self.inputkey) ||
+            item.description.match(_self.inputkey)
+          );
+        });
+      }
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.el-header {
+  background-color: #e8eaeb;
+  height: 68px !important;
+}
+.el-row {
+  padding-bottom: 10px;
+}
+</style>
