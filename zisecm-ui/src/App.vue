b4d7977c4dff5513b031363b915a92c1aefcd7a8
breezerong <nstorm@163.com>
2020-04-20 17:19:18 +0800

    动画

--- zisecm-ui/src/App.vue
+++ zisecm-ui/src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
+    <transition name="rotate-fall">
     <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
+import 'vue-transition.css'
 export default {
   name: "app"
 };
