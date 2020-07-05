c634206c279c0860e17b8764ec98e8536fa1c8ae
breezerong <nstorm@163.com>
2020-07-05 11:47:35 +0800

    同步日志

---
+++ zisecm-portal/src/main/java/com/ecm/cnpe/exchange/service/ISynBatchService.java
@@ -0,0 +1,14 @@
+package com.ecm.cnpe.exchange.service;
+
+import java.util.List;
+
+/**
+ * 
+ * @Title: 同步日志服务
+ * @author Haihong Rong
+ * @date:   2020年7月5日 上午10:36:34 
+ * @Description:
+ */
+public interface ISynBatchService  {
+	
+}
