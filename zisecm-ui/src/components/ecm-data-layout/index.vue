f9a0da8e74d9df0c80d16390e3171df84de88aa8
lanseyouling163 <lanseyouling163@163.com>
2020-07-13 11:15:31 +0800

    数据列表布局，可自动调整高度

---
+++ zisecm-ui/src/components/ecm-data-layout/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <el-container>
+    <el-header>
+      <slot name="header"></slot>
+    </el-header>
+    <el-main>
+      <slot name="main"></slot>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      layout:{
+        height:0,
+        step:250,
+        split:1
+      }
+
+    }
+  },
+  methods:{
+    getLayoutHeight(){
+      this.layout.height = window.innerHeight/this.layout.split
+      return this.layout.height;
+    }
+  },
+  created(){
+    let _self = this
+    window.addEventListener("resize",function(){
+      let size = _self.getLayoutHeight()
+      _self.$emit("onLayoutResize",size)
+    })
+    
+  },
+  mounted(){
+    /**
+     * 跳转至权限提醒页
+     */
+    if(!this.validataPermission()){        
+        let _self=this;
+        _self.$nextTick(()=>{
+            _self.$router.push({ path: '/NoPermission' })
+        })
+        
+    }
+
+    window.addEventListener("resize",function(){
+      let size = _self.getLayoutHeight()
+      _self.$emit("onLayoutResize",size)
+    })
+  }
+}
+</script>
+
+<style scoped>
+.el-header{
+    height: auto;
+}
+</style>
