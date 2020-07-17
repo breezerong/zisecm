40546d2c8a3aa304e0996d1074dd3de422b876dc
lanseyouling163 <lanseyouling163@163.com>
2020-07-17 16:16:37 +0800

    Update IEDController.java

--- zisecm-portal/src/main/java/com/ecm/cnpe/exchange/controller/IEDController.java
+++ zisecm-portal/src/main/java/com/ecm/cnpe/exchange/controller/IEDController.java
@@ -1,14 +1,8 @@
 package com.ecm.cnpe.exchange.controller;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,22 +13,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.alibaba.druid.util.StringUtils;
-import com.ecm.cnpe.exchange.controller.param.DocParam;
-import com.ecm.common.util.DateUtils;
-import com.ecm.common.util.EcmStringUtils;
-import com.ecm.common.util.ExcelUtil;
 import com.ecm.common.util.FileUtils;
 import com.ecm.common.util.JSONUtils;
 import com.ecm.core.ActionContext;
-import com.ecm.core.cache.manager.CacheManagerOper;
 import com.ecm.core.entity.EcmContent;
 import com.ecm.core.entity.EcmDocument;
-import com.ecm.core.entity.EcmGridView;
-import com.ecm.core.entity.EcmGridViewItem;
-import com.ecm.core.entity.LoginUser;
-import com.ecm.core.exception.AccessDeniedException;
-import com.ecm.core.exception.EcmException;
 import com.ecm.core.service.DocumentService;
 import com.ecm.portal.controller.ControllerAbstract;
 
@@ -70,7 +53,7 @@ public class IEDController  extends ControllerAbstract  {
 	/**
 	 * 
 	 * @Title: changeIED
-	 * @author lsyl
+	 * @author Chen Shuo
 	 * @date:  2020年7月17日 上午10:39:19
 	 * @Description IED变更
 	 * @param ids
@@ -79,12 +62,12 @@ public class IEDController  extends ControllerAbstract  {
 	@PostMapping("/exchange/ied/changeIED")
 	@ResponseBody
 	public Map<String, Object> changeIED(@RequestBody List<String> ids){
-		Map<String, Object> result = new HashMap<String, Object>();		
+		Map<String, Object> result = new HashMap<String, Object>();
 		for (String id : ids) {
 			try {
 				EcmDocument docObj = documentService.getObjectById(getToken(), id);
 				EcmDocument checkInDoc = documentService.checkIn(getToken(), id, null, docObj.isCurrent());
-				documentService.updateStatus(getToken(), id, "变更中");		
+				documentService.updateStatus(getToken(), docObj.getId(), "变更中");		
      			checkInDoc.addAttribute("C_ITEM_STATUS1", "修订");
 				checkInDoc.addAttribute("C_IS_RELEASED", 0);
 				checkInDoc.setCurrent(false);
