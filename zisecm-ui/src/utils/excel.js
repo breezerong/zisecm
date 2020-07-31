546d29ee0d25ca7c96859d43b28387df693354db
includeBD <1658209874@qq.com>
2020-07-31 11:00:28 +0800

    IED报表图标栏及操作功能栏修正

--- zisecm-ui/src/utils/excel.js
+++ zisecm-ui/src/utils/excel.js
@@ -5,9 +5,8 @@ var excel = {
      gridName,lang,condition,folderId,filename,sheetname,String orderBy;
     }
    */
-  export:function(params){
-    let url = "/exchange/doc/export"
-    axios.post(url,params,{ responseType: "blob"}).then(function(res){
+  exportTC: function (map, url) {
+    axios.post(dataUrl, JSON.stringify(m), { responseType: "blob" }).then(function (res) {
       console.log(res)
       let fileName = res.headers["content-disposition"].split(";")[1].split("=")[1].replace(/\"/g, "");
       let type = res.headers["content-type"];
@@ -23,13 +22,14 @@ var excel = {
         //释放内存
         window.URL.revokeObjectURL(link.href);
       }
-    }).catch(function(error){
+    }).catch(function (error) {
       console.log(error)
     })
   },
-  export4Cnpe:function(params){
-    let url = params.URL;//"/exchange/doc/export"
-    axios.post(url,params,{ responseType: "blob"}).then(function(res){
+
+  export: function (params) {
+    let url = "/exchange/doc/export"
+    axios.post(url, params, { responseType: "blob" }).then(function (res) {
       let fileName = res.headers["content-disposition"].split(";")[1].split("=")[1].replace(/\"/g, "");
       let type = res.headers["content-type"];
       let blob = new Blob([res.data], { type: type });
@@ -44,7 +44,28 @@ var excel = {
         //释放内存
         window.URL.revokeObjectURL(link.href);
       }
-    }).catch(function(error){
+    }).catch(function (error) {
+      console.log(error)
+    })
+  },
+  export4Cnpe: function (params) {
+    let url = params.URL;//"/exchange/doc/export"
+    axios.post(url, params, { responseType: "blob" }).then(function (res) {
+      let fileName = res.headers["content-disposition"].split(";")[1].split("=")[1].replace(/\"/g, "");
+      let type = res.headers["content-type"];
+      let blob = new Blob([res.data], { type: type });
+      // IE
+      if (window.navigator.msSaveBlob) {
+        window.navigator.msSaveBlob(blob, fileName);
+      } else {
+        var link = document.createElement("a");
+        link.href = window.URL.createObjectURL(blob);
+        link.download = fileName;
+        link.click();
+        //释放内存
+        window.URL.revokeObjectURL(link.href);
+      }
+    }).catch(function (error) {
       console.log(error)
     })
   },
