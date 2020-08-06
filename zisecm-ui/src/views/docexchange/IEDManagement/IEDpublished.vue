2822972ed9c69130c77d21b55a9321193af628d4
lanseyouling163 <lanseyouling163@163.com>
2020-08-06 16:37:54 +0800

    Update IEDpublished.vue

--- zisecm-ui/src/views/docexchange/IEDManagement/IEDpublished.vue
+++ zisecm-ui/src/views/docexchange/IEDManagement/IEDpublished.vue
@@ -293,7 +293,7 @@ export default {
                 })
                 k1+=" AND (" + orS + ")"
             }
-            if(_self.forms.headForm.project != undefined && _self.forms.headForm.project.length>0){
+            if(_self.forms.headForm.project != undefined){
                 k1+=" AND C_PROJECT_NAME in ("+_self.forms.headForm.project +")"
             }
 
