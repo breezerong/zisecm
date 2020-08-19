2712b5e3effc09720847cf336984a4fe0daf9e76
CathayPacificsA333X <944495149@qq.com>
2020-08-19 16:27:04 +0800

    IED接收逻辑更新，现在已经可以正常修改“变更中”的IED了

--- zisecm-portal/src/main/java/com/ecm/cnpe/exchange/service/LogicOption4CnpeIED.java
+++ zisecm-portal/src/main/java/com/ecm/cnpe/exchange/service/LogicOption4CnpeIED.java
@@ -2,7 +2,9 @@ package com.ecm.cnpe.exchange.service;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -30,20 +32,18 @@ public class LogicOption4CnpeIED {
 	private DocumentService documentService;
 	
 	@Transactional(rollbackFor = Exception.class)
-	public boolean IEDOption(String token,EcmDocument mainDoc) throws EcmException{
-	List<EcmDocument> L1 = new ArrayList<EcmDocument>();							//从数据库中取旧版该文件的所有隶属文件
+	public boolean IEDOption(String token,EcmDocument mainDoc) throws EcmException, SqlDeniedException{						//从数据库中取旧版该文件的所有隶属文件
+	List<EcmDocument> temp = new ArrayList<EcmDocument>();
 	EcmDocument tempE1 = new EcmDocument();
 	String coding = mainDoc.getCoding();
-	String condition = "TYPE_NAME='IED' AND CODING= '" + coding +"'"+"AND IS_CURRENT=0";	
-	try {
-		L1 = documentService.getAllObject(condition);
-	} catch (AccessDeniedException | NoPermissionException e1) {
-		// TODO Auto-generated catch block
-		e1.printStackTrace();
-	}
-
-	
-		
+	String condition = "TYPE_NAME='IED' AND CODING= '" + coding +"'"+"AND IS_CURRENT=1 AND STATUS = '变更中'";
+	temp = documentService.getObjects(token, condition);
+	System.out.println(temp.size());
+	for(int i = 0;i < temp.size();i++) {
+		tempE1 = temp.get(i);
+	}   
+		tempE1.addAttribute("IS_CURRENT", 0);
+		tempE1.addAttribute("STATUS", "已变更");
 		try {				//修改当前版本
 			LoginUser user;
 			user = documentService.getCurrentUser(token);
@@ -52,7 +52,9 @@ public class LogicOption4CnpeIED {
 			mainDoc.addAttribute("IS_CURRENT",1);
 			mainDoc.addAttribute("C_ITEM_DATE", d1);
 			mainDoc.addAttribute("C_RECEIVE_DATE", d1);
+			System.out.println(tempE1.getAttributeValue("STATUS"));
 			mainDoc.addAttribute("C_RECEIVER",user.getUserName() );
+			documentService.updateObject(token,tempE1, null);
 			documentService.updateObject(token, mainDoc, null);
 		}
 		catch (Exception e) {
