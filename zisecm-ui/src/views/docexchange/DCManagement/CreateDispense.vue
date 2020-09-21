80220bfabb79f6e3ba6cacac6a51ec53dbb55e56
jing jing <1774330547@qq.com>
2020-09-21 15:40:14 +0800

    材料变更清单，会议纪要内容项Excel导入模板路径修改

--- zisecm-ui/src/views/docexchange/DCManagement/CreateDispense.vue
+++ zisecm-ui/src/views/docexchange/DCManagement/CreateDispense.vue
@@ -324,7 +324,7 @@
                                         <el-button type="primary" @click="beforeCreateDocItem('会议纪要内容项','会议纪要内容项')">{{$t('application.new')}}</el-button>
                                         </el-form-item>
                                         <el-form-item>
-                                        <el-button type="primary" @click="beforMeetMaterialImport($refs.MeetDoc,true,'会议纪要内容项','/系统配置/导入模板/文函/会议纪要内容项')">{{$t('application.Import')}}</el-button>
+                                        <el-button type="primary" @click="beforMeetMaterialImport($refs.MeetDoc,true,'会议纪要内容项','/系统配置/导入模板/会议纪要内容项')">{{$t('application.Import')}}</el-button>
                                         </el-form-item>
                                         <!-- <el-form-item>
                                         <el-button type="primary" @click="beforImport($refs.relevantDoc,true,'相关文件')">{{$t('application.Import')}}</el-button>
@@ -364,7 +364,7 @@
                                         <el-button type="primary" @click="beforeCreateDocItem('材料变更清单','材料变更清单')">{{$t('application.new')}}</el-button>
                                         </el-form-item>
                                         <el-form-item>
-                                        <el-button type="primary" @click="beforMeetMaterialImport($refs.MaterialDoc,true,'材料变更清单','/系统配置/导入模板/文函/材料变更清单')">{{$t('application.Import')}}</el-button>
+                                        <el-button type="primary" @click="beforMeetMaterialImport($refs.MaterialDoc,true,'材料变更清单','/系统配置/导入模板/材料变更清单')">{{$t('application.Import')}}</el-button>
                                         </el-form-item>
                                         <!-- <el-form-item>
                                         <el-button type="primary" @click="beforImport($refs.relevantDoc,true,'相关文件')">{{$t('application.Import')}}</el-button>
