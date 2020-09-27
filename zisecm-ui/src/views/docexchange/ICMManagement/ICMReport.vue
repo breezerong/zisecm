9befaa7fee83cead3dbe49ced279533d6d85f2e2
includeBD <1658209874@qq.com>
2020-09-27 14:39:26 +0800

    国际化（问题反馈、ICM基本报表）

--- zisecm-ui/src/views/docexchange/ICMManagement/ICMReport.vue
+++ zisecm-ui/src/views/docexchange/ICMManagement/ICMReport.vue
@@ -3,28 +3,28 @@
     <template v-slot:header style="height: auto"></template>
     <template v-slot:main>
       <el-tabs v-model="activeName">
-        <el-tab-pane label="基本接口报表" name="ICMBase">
+        <el-tab-pane :label="$t('route.ICMBase')" name="ICMBase">
           <ICMBaseReport ></ICMBaseReport>
         </el-tab-pane>
-        <el-tab-pane label="ICM手册导出报表" name="ICMExport">
+        <el-tab-pane :label="$t('route.ICMExport')" name="ICMExport">
           <ICMReportP ></ICMReportP>
         </el-tab-pane>
-        <el-tab-pane label="ICM接口及交换信息完整清单" name="ICMExchange">
+        <el-tab-pane :label="$t('route.ICMExchange')" name="ICMExchange">
           <ICMExchangeP ></ICMExchangeP>
         </el-tab-pane>
-        <el-tab-pane label="接口打开完成情况报表" name="ICMOpen">
+        <el-tab-pane :label="$t('route.ICMOpen')" name="ICMOpen">
           <ICMOpenP ></ICMOpenP>
         </el-tab-pane>
-        <el-tab-pane label="接口关闭完成情况报表" name="ICMClose">
+        <el-tab-pane :label="$t('route.ICMClose')" name="ICMClose">
           <ICMCloseP ></ICMCloseP>
         </el-tab-pane>
-        <el-tab-pane label="接口回复完成情况报表" name="ICMReply">
+        <el-tab-pane :label="$t('route.ICMReply')" name="ICMReply">
           <ICMReplyP ></ICMReplyP>
         </el-tab-pane>
-        <el-tab-pane label="CNPE各所接口完成统计" name="CNPEComplete">
+        <el-tab-pane :label="$t('route.CNPEComplete')" name="CNPEComplete">
           <CNPECompleteReport ></CNPECompleteReport>
         </el-tab-pane>
-        <el-tab-pane label="合同商接口完成统计" name="ContrICM">
+        <el-tab-pane :label="$t('route.ContrICM')" name="ContrICM">
           <ContactorReport ></ContactorReport>
         </el-tab-pane>
       </el-tabs>
