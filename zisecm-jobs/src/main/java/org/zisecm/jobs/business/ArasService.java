92f054aec91cfea1e3930c744920aea05f3f946d
lanseyouling163 <lanseyouling163@163.com>
2020-10-10 21:09:24 +0800

    aras

--- zisecm-jobs/src/main/java/org/zisecm/jobs/business/ArasService.java
+++ zisecm-jobs/src/main/java/org/zisecm/jobs/business/ArasService.java
@@ -3,16 +3,21 @@ package org.zisecm.jobs.business;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.zisecm.jobs.config.ArasConfig;
 import org.zisecm.jobs.config.ArasRsp;
+
+import com.alibaba.fastjson.JSON;
 
 @Service
 public class ArasService {
@@ -21,22 +26,8 @@ public class ArasService {
 	private ArasConfig aras;
 	
 	public void run() {
-		
-		RestTemplate client = new RestTemplate();
-		HttpHeaders headers = new HttpHeaders();
-		MultiValueMap<String, String> params = new LinkedMultiValueMap();
-		params.add("grant_type", "password");
-		params.add("client_id", "IOMApp");
-		params.add("scope", "Innovator");
-		params.add("username", aras.getUsername());
-		params.add("password", stringToMD5(aras.getPassword()));
-		params.add("database", "OBOTService");
-		
-		String url = aras.getBaseUrl()+aras.getTokenUrl();
-		System.out.println(url);
-		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
-		ArasRsp data = client.postForObject(url, request, ArasRsp.class);
-		System.out.println(data);
+		ArasRsp arasRsp = this.getToken();
+		System.out.println(JSON.toJSON(arasRsp));
 	}
 	
 	public String stringToMD5(String plainText) {
@@ -53,4 +44,83 @@ public class ArasService {
         }
         return md5code;
     }
+	
+	private ArasRsp getToken() {
+		RestTemplate client = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type", aras.getGrantType());
+		params.add("client_id", aras.getClientId());
+		params.add("scope", aras.getScope());
+		params.add("username", aras.getUsername());
+		params.add("password", aras.getPassword());
+		params.add("database", aras.getDatabase());
+
+		HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(params, headers);
+		String url = aras.getUrl() + "/OAuthServer/connect/token";
+		ArasRsp data = client.postForObject(url, requestEntity, ArasRsp.class);
+		System.out.println(JSON.toJSONString(data));
+		return data;
+	}
+
+
+	private void getObjects(ArasRsp arasRsp, String type) {
+		RestTemplate client = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+		headers.setBearerAuth(arasRsp.getAccess_token());
+
+		HttpEntity<?> entity = new HttpEntity<>(headers);
+
+		String url = aras.getUrl() + "/server/odata/" + type;
+		HttpEntity<String> response = client.exchange(url, HttpMethod.GET, entity, String.class);
+
+		System.out.println(response.getHeaders());
+		System.out.println(JSON.parseObject(response.getBody()));
+
+	}
+
+
+	private void getObjectById(ArasRsp arasRsp, String type, String id) {
+		RestTemplate client = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+		headers.setBearerAuth(arasRsp.getAccess_token());
+		HttpEntity<?> entity = new HttpEntity<>(headers);
+		String url = aras.getUrl() + "/server/odata/" + type + "('" + id + "')";
+		HttpEntity<String> response = client.exchange(url, HttpMethod.GET, entity, String.class);
+
+		System.out.println(response.getHeaders());
+		System.out.println(JSON.parseObject(response.getBody()));
+
+	}
+
+
+	private void getCount(ArasRsp arasRsp, String type) {
+		RestTemplate client = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+		headers.setBearerAuth(arasRsp.getAccess_token());
+		HttpEntity<?> entity = new HttpEntity<>(headers);
+		String url = aras.getUrl() + "/server/odata/" + type + "/$count";
+		System.out.println(url);
+		HttpEntity<String> response = client.exchange(url, HttpMethod.GET, entity, String.class);
+
+		System.out.println(response.getHeaders());
+		System.out.println(response.getBody());
+
+	}
+
+
+	public void createItem(ArasRsp arasRsp, String type, Map<String, Object> body) {
+		RestTemplate client = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+		headers.setBearerAuth(arasRsp.getAccess_token());
+		HttpEntity<Map<String, Object>> entity = new HttpEntity<>(body, headers);
+		String url = aras.getUrl() + "/server/odata/" + type;
+		System.out.println(url);
+		HttpEntity<String> response = client.exchange(url, HttpMethod.POST, entity, String.class);
+		System.out.println(response.getBody());
+	}
 }
--- zisecm-jobs/src/main/java/org/zisecm/jobs/config/ArasConfig.java
+++ zisecm-jobs/src/main/java/org/zisecm/jobs/config/ArasConfig.java
@@ -1,25 +1,44 @@
 package org.zisecm.jobs.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration
 @ConfigurationProperties(prefix = "aras")
 public class ArasConfig {
-	private String baseUrl="http://121.40.103.187/InnovatorServer";
-	private String username="admin";
-	private String password="innovator";
 	
-	private String serverDiscoveryUrl = "/Server/OAuthServerDiscovery.aspx";
+	private String grantType="password";
 	
-	private String tokenUrl = "/oauthserver/connect/token";
+	private String username = "admin";
+	
+	private String password="607920b64fe136f9ab2389e371852af2";
 
-	public String getBaseUrl() {
-		return baseUrl;
+	private String clientId="IOMApp";
+	
+	private String scope="Innovator";
+	
+	private String database="InnovatorSolutions";
+	
+	private String url="http://121.40.103.187/InnovatorServer";
+	
+	private String tokenUrl="/OAuthServer/connect/token";
+	
+	public String getGrantType() {
+		return grantType;
 	}
 
-	public void setBaseUrl(String baseUrl) {
-		this.baseUrl = baseUrl;
+	public void setGrantType(String grantType) {
+		this.grantType = grantType;
+	}
+	
+	
+
+	public String getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
 	}
 
 	public String getUsername() {
@@ -38,12 +57,28 @@ public class ArasConfig {
 		this.password = password;
 	}
 
-	public String getServerDiscoveryUrl() {
-		return serverDiscoveryUrl;
+	public String getScope() {
+		return scope;
 	}
 
-	public void setServerDiscoveryUrl(String serverDiscoveryUrl) {
-		this.serverDiscoveryUrl = serverDiscoveryUrl;
+	public void setScope(String scope) {
+		this.scope = scope;
+	}
+
+	public String getDatabase() {
+		return database;
+	}
+
+	public void setDatabase(String database) {
+		this.database = database;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
 	}
 
 	public String getTokenUrl() {
@@ -54,4 +89,5 @@ public class ArasConfig {
 		this.tokenUrl = tokenUrl;
 	}
 
+	
 }
