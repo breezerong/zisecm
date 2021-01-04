db42bd3629832e66c7274542577ea97cf9b5e4b3
breezerong <nstorm@163.com>
2021-01-04 17:00:47 +0800

    自定义日期默认启用

--- zisecm-core/src/main/java/com/ecm/core/util/AuditUtils.java
+++ zisecm-core/src/main/java/com/ecm/core/util/AuditUtils.java
@@ -7,15 +7,15 @@ import com.ecm.core.entity.EcmSystemEvent;
 
 public class AuditUtils {
 	public static boolean isEnabled(String eventName) {
-		if(eventName != null && !eventName.startsWith("ecm")) {
-			return true;
-		}
 		Map<String, EcmSystemEvent>  map =CacheManagerOper.getEcmSystemEvents();
 		if(map!=null) {
 			EcmSystemEvent event = map.get(eventName);
 			if(event !=null ) {
 				return event.getEnabled();
 			}
+		}
+		if(eventName != null && !eventName.startsWith("ecm")) {
+			return true;
 		}
 		return false;
 	}
