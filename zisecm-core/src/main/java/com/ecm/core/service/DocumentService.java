cd540ee6dbf1f5d978b0d5d2723b54f70eb18c03
Jing_707 <1774330547@qq.com>
2021-05-24 14:40:57 +0800

    未生成编码不进行唯一性校验

--- zisecm-core/src/main/java/com/ecm/core/service/DocumentService.java
+++ zisecm-core/src/main/java/com/ecm/core/service/DocumentService.java
@@ -898,7 +898,8 @@ public class DocumentService extends EcmObjectService<EcmDocument> implements ID
 	@Transactional(rollbackFor = Exception.class)
 	public void updateObject(String token, EcmDocument doc, EcmContent content) throws NoPermissionException, AccessDeniedException, EcmException {
 		try {
-			if(!doc.getCoding().equals("@Sequence")) {
+			String coding=doc.getCoding();
+			if(coding!=null&&!coding.equals("@Sequence")) {
 				this.validate(token, doc.getAttributes());
 			}
 		} catch (AccessDeniedException e) {
